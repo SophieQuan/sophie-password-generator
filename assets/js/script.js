bb57713fcbe07e5979e71bcea322fd29d8715124
SophieQuan <sophiequan7@gmail.com>
2022-01-15 00:06:17 -0500

    add verify password function and generate password funtion. Ready to run

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,5 @@
 // Assignment code here
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 //SET UP 4 TYPE OF CHARACTERS and LENGTH FOR THE PASSWORD
 //----lowercase characters array
 var lowercaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -22,6 +20,10 @@ var passLengthFunc = function(){
 
 //FUNCTION VERIFY PASSWORD INFO whether or not to include lowercase, uppercase, numeric, and/or special characters
 function verifyPassword (){
+  //confirm password length by execute passLengthFunc()
+  passLengthFunc();
+
+
   if(passwordLength < 8 || passwordLength > 128){
     alert("Password length must be at least 8 characters and no more than 128 characters.");
     generatePassword();
@@ -29,11 +31,79 @@ function verifyPassword (){
   else if( isNaN(passwordLength) ){
     alert("Invalid input. Please insert a number.");
     generatePassword();
-  } 
-  else{
-    generatePassword();
-  };
+  }
 
+  //confirm includding lowercase characters
+  var confirmlowerChar = window.confirm("Would you like to include lowercase characters? Click Ok to confirm.");
+
+  //confirm includding lowercase characters
+  var confirmUpperChar = window.confirm("Would you like to include uppercase characters? Click Ok to confirm.");
+
+  //confirm includding lowercase characters
+  var confirmNumChar = window.confirm("Would you like to include number characters? Click Ok to confirm.");
+
+  //confirm includding lowercase characters
+  var confirmSpecialChar = window.confirm("Would you like to include special characters? Click Ok to confirm.");
+
+  //user should confirm at least one character type, show alert if not meet this requirement
+  if(!confirmlowerChar && !confirmUpperChar && !confirmNumChar && !confirmSpecialChar){
+    alert ("At least one character type should be selected. Please try again.");
+  }
+
+  // put all password input in object passwordInput
+  var passwordInput = {
+    passwordLength: passwordLength,
+    confirmlowerChar: confirmlowerChar,
+    confirmUpperChar: confirmUpperChar,
+    confirmNumChar: confirmNumChar,
+    confirmSpecialChar: confirmSpecialChar,
+  }
+  return passwordInput;
+};
+
+function generatePassword(){
+  //declare the verifyPassword function
+  var verifiyPassword = verifyPassword();
+
+  //declare and store final password in an empty string
+  var finalPassword = "";
+
+  //create an array stores all confirm character include in the final password
+  var confirmIncludeChar = [];
+
+  //confirm character condition
+  //----if include lowercase characters - add to final password
+  if (verifiyPassword.confirmlowerChar) {
+    confirmIncludeChar = confirmIncludeChar.concat(lowercaseChar);
+  }
+
+  //----if include uppercase characters - add to final password
+  if (verifiyPassword.confirmUpperChar) {
+    confirmIncludeChar = confirmIncludeChar.concat(uppercaseChar);
+  }
+
+  //----if include number characters - add to final password
+  if (verifiyPassword.confirmNumChar) {
+    confirmIncludeChar = confirmIncludeChar.concat(numbersChar);
+  }
+
+  //----if include uppercase characters - add to final password
+  if (verifiyPassword.confirmSpecialChar) {
+    confirmIncludeChar = confirmIncludeChar.concat(specialChar);
+  }
+
+  console.log(confirmIncludeChar);
+
+  //generate random character 
+  confirmIncludeChar = confirmIncludeChar.sort(() => Math.floor(Math.random() - 0.5));
+  console.log(confirmIncludeChar);
+
+  //only return the input password length
+  finalPassword = confirmIncludeChar.slice(0, passwordLength);
+  console.log(finalPassword);
+  
+  return finalPassword.join("");
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
